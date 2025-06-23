60549f2e63c124c1427c24fb8b0572e188b4ec37
Scott Germain <you@example.com>
2025-06-23 13:22:32 +1000

    cleanup xv2-2t0 prints

--- cambium/rcp/xv2-2t0.py
+++ cambium/rcp/xv2-2t0.py
@@ -46,34 +46,29 @@ conn = Connection(host=hostname, user=username, connect_kwargs={"password": pass
     
 os.system("sudo ip address add 192.168.0.100/24 dev eth0 > /dev/null 2>&1") #Add IP for connectivity to default IP 192.168.0.1
 
-print (Fore.GREEN + "Checking for factory default XV2 connected on 192.168.0.1")
+print ("Checking for factory default XV2 connected on 192.168.0.1")
 print ("To factory reset, hold reset button for 15 seconds then release. Green light will go off after a few seconds then should return to default configuration")
 while True:
 	process = subprocess.Popen(['ping','-c' ,'1', '192.168.0.1'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	stdout, stderr = process.communicate()
 	returncode = process.returncode
 	if returncode == 0:
-		print (Fore.GREEN + "192.168.0.1 online")
+		print ("192.168.0.1 online")
 		print ('\a')
 		break
-	print(Fore.RED + "192.168.0.1 not responding...")
+	print("192.168.0.1 not responding...")
 
-print (Fore.GREEN + "Checking Hardware")
+print ("Checking Hardware")
 stream = os.popen('snmpget -v 2c -c public 192.168.0.1 .1.3.6.1.4.1.17713.22.1.1.1.5.0')
 output = stream.read()
 
 if 'XV2-2T0' in output:
-        print(Fore.GREEN + "Confirmed XV2-2T0")
+        print("Confirmed XV2-2T0")
         hardware = "XV2-2T0"
 else:
-        print(Fore.RED + "Not a XV2-2T0")
-        print(Fore.RED + "Check hardware model and that you're running the right config file")
+        print("Not a XV2-2T0")
+        print("Check hardware model and that you're running the right config file")
         quit()
-
-#community_name = input("Enter Community Name: ")
-#community_name = community_name.replace(" ", "_")
-#device_name = input("Enter Device Name: ")
-#ip_address = input("Enter IP Address: ")
 
 #Create config file from base template
 config_pi_ip = '192.168.0.50'
@@ -93,14 +88,14 @@ try:
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(config_pi_ip, username=pi_username, password=pi_password)
     stdin, stdout, stderr = client.exec_command(command)
-    print(f"Created config {custom_config}")
+    #print(f"Created config {custom_config}")
 
 finally:
     client.close()
 
 #Firmware - upgrade to 6.6.0.3-r9 if necessary.
 while True:
-    print(Fore.GREEN + "Checking current firmware version...")
+    #print(Fore.GREEN + "Checking current firmware version...")
     firmware = functions.snmpgetpublic("192.168.0.1", ".1.3.6.1.4.1.17713.22.1.1.1.8.0")
 
     if '6.6.0.3-r9' in firmware:
@@ -124,20 +119,20 @@ while True:
         time.sleep(1) #Time delay to apply config before rebooting
         break
     else:
-        print(firmware)
-        print(Fore.RED + "Need to update to v6.6.0.3-r9")
+        #print(firmware)
+        #print(Fore.RED + "Need to update to v6.6.0.3-r9")
         print(Fore.GREEN + "Updating firmware to v6.6.0.3-r9")
         
         #Update firmware
-        print("Updating firmware")
+        #print("Updating firmware")
         first_command = 'upgrade http://192.168.0.50/firmware/xv22_xv22t0_xe34_xv22t1_xe34tn-6.6.0.3-r9.cimg verbose'
         handle_prompts(conn, first_command)
 
         # Update config
         second_command = f'import config {custom_config}'
-        print("Waiting 5s before uploading config")
-        time.sleep(5)
         print("Updating configuration file")
+        time.sleep(5)
+
         # The prompt string occasionally differs slightly between firmware
         # versions, so use a loose regex to match anything that ends with the
         # standard "(y/n)" question.
@@ -148,6 +143,9 @@ while True:
         )
         config_response = handle_prompts(conn, second_command, watchers=[enter_responder])
         print("Updated Config")
+        time.sleep(5) #Wait for reboot to begin before starting ping
+        print("Reminder to turn printer on - make sure 'Editor Lite' LED is off")
+        time.sleep(5) #Wait for reboot to begin before starting ping
         print(config_response.stdout)
         # Loop until 'Error' is not found in the output
         while 'Error' in config_response.stdout:
@@ -184,49 +182,24 @@ while True:
         #finally:
         #    conn.close()
         #time.sleep(3)
-        print(Fore.GREEN + "Waiting for device to come back online...")
+        #print(Fore.GREEN + "Waiting for device to come back online...")
         print("Reminder to turn printer on - make sure 'Editor Lite' LED is off")
-        time.sleep(10) #Wait for reboot to begin before starting ping
+        time.sleep(15) #Wait for reboot to begin before starting ping
         while True:
                 process = subprocess.Popen(['ping','-c' ,'1', ip_address], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 stdout, stderr = process.communicate()
                 returncode = process.returncode
                 if returncode == 0:
-                    print (Fore.GREEN + f"{ip_address} online")
+                    #print (Fore.GREEN + f"{ip_address} online")
                     break
                 print(Fore.RED + f"{ip_address} not responding...")
-        print (Fore.GREEN + f"XV2 back online as {device_name} at {ip_address}")
+        #print (Fore.GREEN + f"XV2 back online as {device_name} at {ip_address}")
         break
 
 #Print labels for the devices
 current_date = datetime.now()
 date = current_date.strftime("%d/%m/%Y")
-print(Fore.GREEN + "Printing label for XV2 radio")
+#print(Fore.GREEN + "Printing label for XV2 radio")
 print_v1.pass_label(device_name,ip_address,f"PASS {date}")
 print_v1.pass_label(device_name,ip_address,f"PASS {date}")
-
-print (Fore.MAGENTA + "***********************************************Finished!***********************************************")
-
-""" #Play alert bell
-print ('\a')
-time.sleep(1)
-print ('\a')
-time.sleep(1)
-print ('\a')
-time.sleep(1)
-print ('\a')
-
-print ('Restart? Plug in another Cambium XV2 in with default IP')
-print ('Or press Ctrl+Z to exit')
-
-
-while True:
-    process = subprocess.Popen(['ping','-c' ,'1', '192.168.0.1'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    returncode = process.returncode
-    if returncode == 0:
-        quit()    
-    print(Fore.RED + "192.168.0.1 not responding...")
-    if returncode == 0:
-        os.execl(sys.executable, sys.executable, *sys.argv)
-    print(Fore.MAGENTA + "Plug another XV2 in or press ctrl+z to quit.... checking again") """
+#print (Fore.MAGENTA + "***********************************************Finished!***********************************************")
