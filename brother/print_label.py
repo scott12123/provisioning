2afb2c9ec89222f1512098b8f697fee0f013e86c
scott12123 <92611452+scott12123@users.noreply.github.com>
2025-06-25 18:15:37 +1000

    Fix barcode preview and wrap label text

--- brother/print_label.py
+++ brother/print_label.py
@@ -5,6 +5,7 @@ import os
 import logging
 from io import BytesIO
 from PIL import Image, ImageDraw, ImageFont
+from textwrap import wrap
 from typing import Optional
 from brother_ql import BrotherQLRaster, create_label
 from brother_ql.backends.helpers import send
@@ -27,27 +28,69 @@ def build_image(text: str, barcode: Optional[str]) -> Image.Image:
     img = Image.new('L', (566, 165), color=255)
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype(FONT_PATH, 40)
-    draw.text((5, 60), text, font=font, fill=0)
+
+    text_area_width = 420
+    lines = []
+    for para in text.splitlines() or ['']:
+        if not para:
+            lines.append('')
+            continue
+        words = para.split()
+        if not words:
+            lines.append('')
+            continue
+        cur = words[0]
+        for word in words[1:]:
+            test = f"{cur} {word}"
+            if draw.textlength(test, font=font) <= text_area_width:
+                cur = test
+            else:
+                lines.append(cur)
+                cur = word
+        lines.append(cur)
+
+    line_height = font.getbbox('Ag')[3] - font.getbbox('Ag')[1]
+    total_height = line_height * len(lines)
+    y = (img.height - total_height) // 2
+    for line in lines:
+        draw.text((5, y), line, font=font, fill=0)
+        y += line_height
 
     max_height = img.height - 20
+    barcode_x = 430
+    available_width = img.width - barcode_x
 
     if barcode == 'qr':
+        size = min(max_height, available_width)
         qr_img = qrcode.make(text)
-        qr_img = qr_img.resize((max_height, max_height))
-        img.paste(qr_img, (430, (img.height - qr_img.height) // 2))
+        qr_img = qr_img.resize((size, size))
+        img.paste(qr_img, (barcode_x + (available_width - size) // 2,
+                          (img.height - size) // 2))
     elif barcode == 'data_matrix':
         encoder = DataMatrixEncoder(text)
         dm_img = Image.open(BytesIO(encoder.get_imagedata()))
-        dm_img = dm_img.resize((max_height, max_height))
-        img.paste(dm_img, (430, (img.height - dm_img.height) // 2))
+        size = min(max_height, available_width)
+        dm_img = dm_img.resize((size, size))
+        img.paste(dm_img, (barcode_x + (available_width - size) // 2,
+                           (img.height - size) // 2))
     elif barcode == 'code128':
         bc_cls = get_barcode_class('code128')
         bc = bc_cls(text, writer=ImageWriter())
         bc_img = bc.render(writer_options={'module_height': max_height})
-        ratio = max_height / bc_img.height
-        width = int(bc_img.width * ratio)
-        bc_img = bc_img.resize((width, max_height), Image.ANTIALIAS).convert('L')
-        img.paste(bc_img, (430, (img.height - max_height) // 2))
+        if bc_img.width > available_width:
+            ratio = available_width / bc_img.width
+            new_width = available_width
+            new_height = int(bc_img.height * ratio)
+        else:
+            new_width = bc_img.width
+            new_height = bc_img.height
+        if new_height > max_height:
+            ratio = max_height / new_height
+            new_width = int(new_width * ratio)
+            new_height = max_height
+        bc_img = bc_img.resize((new_width, new_height), Image.LANCZOS).convert('L')
+        img.paste(bc_img, (barcode_x + (available_width - new_width) // 2,
+                          (img.height - new_height) // 2))
 
     return img
 
